1d3a50e2769f1f155f5bc1dd2872c5a15c57d970
mauricioangarita1094-arch <mauricioangarita1094@gmail.com>
2025-10-11 12:31:17 -0500

    Actualizado en index.html y Datadog

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,52 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare const datadogLogs: any;
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('Proyecto_class_vercel');
+
+  constructor() {}
+
+  logInfo(message: string):{
+    if (typeof DD_RUM === 'undefined') {
+    DD_RUM.addAction(message, {  level: 'info', module: 'AppComponent' });
+    } else {
+      console.warn('DD_RUM no está definido:', message);
+    }
+  }
+
+  logError(message: string, error: any):{
+    if (typeof DD_RUM === 'undefined') {
+     DD_RUM.addError (message, { error, level: 'error', module: 'AppComponent' });
+    } else {
+      console.warn('DD_RUM no está definido:', message, error);
+    }
+  }
+
+
+  ngAfterViewInit(): {
+  setTimeout(() => {
+    if (typeof DD_RUM !== 'undefined') {
+      DD_RUM.addAction('Aplicacion Angular iniciada correctamente');
+      } 
+      },  1000); //espera 1 segundo
+    }
+
+    
+    // Simulación de error
+    simulateError() {
+      try {
+        throw new Error('Error simulado en la aplicación Angular');
+        } catch (e) {
+          this.logError('Se produjo un error simulado', e);
+        }
+    }
 }
